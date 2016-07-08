de53260a8217ac950a6834e1805fee74214f7218
Joakim Friberg <joakim.friberg@gmail.com>
2016-07-08 18:00:10 +0200

    Add sign in by form functionality

--- RedeyeStormpathBundle.php
+++ RedeyeStormpathBundle.php
@@ -5,6 +5,7 @@ namespace Redeye\StormpathBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Redeye\StormpathBundle\DependencyInjection\Security\Factory\StormpathHttpBasicFactory;
+use Redeye\StormpathBundle\DependencyInjection\Security\Factory\StormpathFormLoginFactory;
 
 class RedeyeStormpathBundle extends Bundle
 {
@@ -15,6 +16,7 @@ class RedeyeStormpathBundle extends Bundle
         if ($container->hasExtension('security')) {
             $extension = $container->getExtension('security');
             $extension->addSecurityListenerFactory(new StormpathHttpBasicFactory());
+            $extension->addSecurityListenerFactory(new StormpathFormLoginFactory());
         }
     }
 }
