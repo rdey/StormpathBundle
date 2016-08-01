ef04eded9f97985e810ebe2cf6ff614730b2f7d1
Nicholas Ruunu <nicholas@ruu.nu>
2016-08-01 18:34:07 +0200

    Fixes new configuration

--- DependencyInjection/RedeyeStormpathExtension.php
+++ DependencyInjection/RedeyeStormpathExtension.php
@@ -2,6 +2,10 @@
 
 namespace Redeye\StormpathBundle\DependencyInjection;
 
+use Stormpath\Cache\ArrayCacheManager;
+use Stormpath\Cache\MemcachedCacheManager;
+use Stormpath\Cache\NullCacheManager;
+use Stormpath\Cache\RedisCacheManager;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -34,8 +38,6 @@ class RedeyeStormpathExtension extends Extension
         $container->setParameter('redeye_stormpath.api_key.id_property_name', $config['client']['id_property_name']);
         $container->setParameter('redeye_stormpath.api_key.secret_property_name', $config['client']['secret_property_name']);
         $container->setParameter('redeye_stormpath.api_key.api_key_file', $config['client']['api_key_file']);
-
-        $container->setParameter('redeye_stormpath.client.cache_manager_class', $config['client']['cache_manager']);
 
         if (isset($config['resource_registries'])) {
             $groupHrefRegistryDefinition = $container->getDefinition('redeye_stormpath.resource_registry.group_href');
@@ -81,12 +83,28 @@ class RedeyeStormpathExtension extends Extension
     {
         $factoryDef = $container->getDefinition('redeye_stormpath.client');
 
-        if (isset($config['client']['cache_service'])) {
-            $factoryDef->replaceArgument(1, new Definition(PSR6InstanceCacheManager::class, [new Reference($config['client']['cache_service'])]));
+        switch ($config['cache']['type']) {
+            case 'memcached':
+                $factoryDef->replaceArgument(1, new Definition(MemcachedCacheManager::class));
+                break;
+
+            case 'redis':
+                $factoryDef->replaceArgument(1, new Definition(RedisCacheManager::class));
+                break;
+
+            case 'array':
+                $factoryDef->replaceArgument(1, new Definition(ArrayCacheManager::class));
+                break;
+
+            case 'service':
+                $factoryDef->replaceArgument(1, new Definition(PSR6InstanceCacheManager::class, [new Reference($config['service'])]));
+                break;
+
+            case 'null':
+                $factoryDef->replaceArgument(1, new Definition(NullCacheManager::class));
+                break;
         }
 
-        if (isset($config['client']['cache_manager_options'])) {
-            $factoryDef->replaceArgument(2, $config['client']['cache_manager_options']);
-        }
+        $factoryDef->replaceArgument(2, $config['cache']);
     }
 }
